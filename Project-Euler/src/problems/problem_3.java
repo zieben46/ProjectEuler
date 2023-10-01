d395988f7e1448ba4948abcaf4022b8fbbe8b777
Nick Ziebert <zieben46@gmail.com>
2023-10-01 11:30:14 -0400

    Add problem_3

---
+++ Project-Euler/src/problems/problem_3.java
@@ -0,0 +1,18 @@
+package problems;
+
+public class problem_3 {
+	public static void main(String[] args) {
+		System.out.println(breakdown(600851475143L));
+	}
+
+	public static long breakdown(long input) {
+		for (long i=(input-1)/1000000;i>1;i=i-1) {
+			System.out.println(i);
+			if (input%i==0) {
+				input=i;
+				breakdown(input);
+			}
+		}
+		return input;
+	}
+}
