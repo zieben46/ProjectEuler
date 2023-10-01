fc27c0159bccc9542aebb972c9d8a2e7b35dd95b
Nick Ziebert <zieben46@gmail.com>
2023-10-01 10:59:23 -0400

    Initial commit

---
+++ Project-Euler/src/problems/problem_1.java
@@ -0,0 +1,20 @@
+package problems;
+
+public class problem_1 {
+	
+	public static int answer;
+	public static void main(String[] args) {
+		
+		//System.out.println(Int.testIt(1));
+		
+		for (int i=1;i<1000;i++) {
+			answer=answer+testIt(i);	
+		}
+		System.out.println(answer);
+	}
+	
+	public static int testIt(int i) {
+		return (i%3==0 || i%5==0 ? i:0);
+	}
+
+}
---
+++ Project-Euler/src/problems/problem_2.java
@@ -0,0 +1,31 @@
+package problems;
+
+public class problem_2 {
+
+	
+	public static void main(String[] args) {
+		int answer=0;
+		for (int i=1;i<=1000000;i++) {
+			if (testEven(returnFib(i))>4000000) {
+				System.out.println(answer);
+				break;
+			}
+			answer=answer+testEven(returnFib(i));
+		}
+	}
+
+	public static int returnFib(int input) {
+		int output=1;
+		if (input<=2) {
+			return output;
+		}
+		else {
+			return output=returnFib(input-1)+returnFib(input-2);
+		}
+	}
+
+	public static int testEven(int i){
+		return  (i%2==0 ? i:0);
+	}
+	
+}
